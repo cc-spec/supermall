ff332a72d107ed5b29f11eefafa57458be5416d2
cf <15719192636@163.com>
2020-07-29 19:39:29 +0800

    catedory 获取数据

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,37 +1,74 @@
 <template>
   <div id="category">
     <nav-bar class="nav"><div class="category-nav" slot="left">更多商品</div></nav-bar>
-    <category-tab-control id="category-tab"></category-tab-control>
+    <div class="content">
+      <div class="left">
+        <category-tab-menu id="category-tab" :category="category"/>
+      </div>
+      <div class="right">
+        <div>right</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import CategoryTabControl from './childComps/CategoryTabControl'
+import CategoryTabMenu from './childComps/CategoryTabMenu'
 
 import NavBar from 'components/common/navbar/NavBar'
+
+import {getCategory, getSubcategory, getCategoryDetail} from 'network/category'
 
 export default {
   name: 'Category',
   components: {
-    CategoryTabControl,
+    CategoryTabMenu,
     NavBar
   },
   data() {
-    return {}
+    return {
+      category: []
+    }
+  },
+  created() {
+    this.getCategory()
+  },
+  methods: {
+    /**
+     * 事件监听相关方法
+    **/
+
+    /**
+     * 网络请求相关方法
+    **/
+    getCategory() {
+      getCategory().then(res => {
+        this.category = res.data.category.list
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
   .nav {
+    position: fixed;
+    width: 100vh;
     color: #fff;
     background-color: var(--color-tint);
   }
   .category-nav {
     width: 100px;
+    padding-left: 30px;
   }
   #category #category-tab{
     display: flex;
     flex-direction: column;
+  }
+  #category .content .left{
+    float: left;
+  }
+  #category .content .right {
+    float: right;
   }
 </style>
---
+++ src/views/category/childComps/CategoryTabMenu.vue
@@ -0,0 +1,72 @@
+<template>
+  <scroll class="content">
+    <div class="tab-menu">
+      <div v-for="(item, index) of category" :key="index"
+        class="menu-item"
+        @click="clickMenu(index)"
+        :class="{active: currentIndex === index}">
+        <span>{{item.title}}</span>
+      </div>
+    </div>
+  </scroll>
+</template>
+
+<script>
+import Scroll from 'components/common/scroll/Scroll'
+
+export default {
+  name: 'CategoryTabMenu',
+  components: {
+    Scroll
+  },
+  props: {
+    category: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    clickMenu(index) {
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-menu {
+    height: 100vh;
+    width: 100px;
+  }
+  .menu-item {
+    width: 100px;
+    height: 40px;
+  }
+  .menu-item span {
+    display: block;
+    width: 100px;
+    line-height: 40px;
+    padding-left: 20px;
+  }
+  .active {
+    color: var(--color-tint);
+  }
+  .active span {
+    border-left: 2px solid var(--color-tint);
+  }
+  .content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+    /* overflow: hidden; */
+  }
+</style>
